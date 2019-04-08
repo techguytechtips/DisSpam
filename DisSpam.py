e5a76cfc6da84716c53e9ce98680a185a549b55b
techguytechtips <33581259+techguytechtips@users.noreply.github.com>
2019-04-07 20:08:00 -0400

    Add files via upload
    added an option to include the @ so you can spam other msgs and added the option too have infinite amout of pings.

--- DisSpam.py
+++ DisSpam.py
@@ -12,17 +12,17 @@ class Window(Frame):
 		
 		
 	def init_window(self):
-		
 		self.master.title("DisSpam")
 		self.pack(fill=BOTH, expand=1)
 		
-		runButton = Button(self, text="Spam!", command=self.spam, fg='blue', bg='green')
+		runButton = Button(self, text="Spam!", command=self.spam, fg='#99AAB5', bg='#23272A')
 		runButton.place(x=155, y=210)
+	
 	def spam(self):
 		global i
-		if box.get() == 1:
+		if box.get() == 1 and at.get() == 1 and infinite.get() == 0:
 			l = i.get()
-			g = 1
+			g = 1#2C2F33
 			time.sleep(5)
 			while g <= l:
 				time.sleep(0.9)
@@ -30,24 +30,71 @@ class Window(Frame):
 				pyautogui.typewrite(['enter'])
 				pyautogui.typewrite(['enter'])
 				g += (1)
-		elif box.get() == 0:
+		elif box.get() == 0 and at.get() == 0 and infinite.get() == 0:
 			l = i.get()
 			g = 1
 			time.sleep(5)
+			while g <= l:
+				pyautogui.typewrite((tag.get()), interval=0)
+				pyautogui.typewrite(['enter'])
+				pyautogui.typewrite(['enter'])
+				g += (1)
+		elif box.get() == 1 and at.get() == 0 and infinite.get() == 0:
+			l = i.get()
+			g = 1
+			time.sleep(5)
+			while g <= l:
+				time.sleep(0.9)
+				pyautogui.typewrite((tag.get()), interval=0)
+				pyautogui.typewrite(['enter'])
+				pyautogui.typewrite(['enter'])
+				g += (1)
+		elif box.get() == 0 and at.get() == 1 and infinite.get() == 0:
 			while g <= l:
 				pyautogui.typewrite("@" + (tag.get()), interval=0)
 				pyautogui.typewrite(['enter'])
 				pyautogui.typewrite(['enter'])
 				g += (1)
+		elif box.get() == 1 and at.get() == 1 and infinite.get() == 1:
+			time.sleep(5)#2C2F33
+			while(True):
+				time.sleep(0.9)
+				pyautogui.typewrite("@" + (tag.get()), interval=0)
+				pyautogui.typewrite(['enter'])
+				pyautogui.typewrite(['enter'])
+				
+		elif box.get() == 0 and at.get() == 0 and infinite.get() == 1:
+			time.sleep(5)
+			while(True):
+				pyautogui.typewrite((tag.get()), interval=0)
+				pyautogui.typewrite(['enter'])
+				pyautogui.typewrite(['enter'])
+		elif box.get() == 1 and at.get() == 0 and infinite.get() == 1:
+			time.sleep(5)
+			while(True):
+				time.sleep(0.9)
+				pyautogui.typewrite((tag.get()), interval=0)
+				pyautogui.typewrite(['enter'])
+				pyautogui.typewrite(['enter'])
+		elif box.get() == 0 and at.get() == 1 and infinite.get() == 1:
+			time.sleep(5)
+			while(True):
+				pyautogui.typewrite("@" + (tag.get()), interval=0)
+				pyautogui.typewrite(['enter'])
+				pyautogui.typewrite(['enter'])
 root = Tk()
 
 tag = StringVar()
 
 box = IntVar()
 
+at = IntVar()
+
 i = IntVar()
 
-root.geometry("400x295")
+infinite = IntVar()
+
+root.geometry("400x295")#2C2F33
 
 root.tk.call('wm', 'iconphoto', root._w, PhotoImage(file='myicon.gif'))
 
@@ -55,7 +102,7 @@ app = Window(root)
 
 label = Label(root, text = "DisSpam by TechGuyTechTips", font=("bold", 10),fg="darkred").pack()
 
-label1 = Label(root, text="Enter Discord Tag: @", font=("arial",12, "bold"), fg="black").place(x=15, y=25)
+label1 = Label(root, text="Enter Discord Tag:", font=("arial",12, "bold"), fg="black").place(x=17, y=27)
 
 label2 = Label(root, text="Enter Number of Pings: ", font=("arial",12, "bold"), fg="black").place(x=17, y=60)
 
@@ -63,10 +110,14 @@ label3 = Label(root, text="You Dont have too use full username.", font=("arial",
 
 label4 = Label(root, text="You have 5 seconds to get back into discord before it starts pinging.", font=("arial",8, "bold"), fg="red").place(x=17, y=170)
 
-entry_box = Entry(root, textvariable=(tag), width=25, bg="red").place(x=180, y=25)
+entry_box = Entry(root, textvariable=(tag), width=25, bg="#7289DA").place(x=180, y=25)
 
-entry_box = Entry(root, textvariable=i, width=3, bg="red").place(x=207, y=60)
+entry_box = Entry(root, textvariable=i, width=3, bg="#7289DA").place(x=207, y=60)
 
-Checkbutton(root, text="Bypass Anti-spam", variable=box).place(x=200, y=100)
+Checkbutton(root, text="Bypass Anti-spam", variable=box).place(x=200, y=90)
+
+Checkbutton(root, text="Include @ sign", variable=at).place(x=50, y=90)
+
+Checkbutton(root, text="Infinite pings", variable=infinite).place(x=125, y=126)
 
 root.mainloop()
