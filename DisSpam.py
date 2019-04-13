462bc57dd7da8380a78b12f0efd3436885c1fbc0
techguytechtips <33581259+techguytechtips@users.noreply.github.com>
2019-04-13 17:54:59 -0400

    Add files via upload

--- DisSpam.py
+++ DisSpam.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import pyautogui
 import time
+import threading
+from PIL import ImageTk, Image
 class Window(Frame):                   
 	def __init__(self, master = None):
 		Frame.__init__(self, master)
@@ -17,72 +19,132 @@ class Window(Frame):
 		
 		runButton = Button(self, text="Spam!", command=self.spam, fg='#99AAB5', bg='#23272A')
 		runButton.place(x=155, y=210)
-	
+		runButton2 = Button(self, text="Stop", command=self.stap, fg='#23272A', bg='red')
+		runButton2.place(x=335, y=225)
+	def stap(self):
+		global stp
+		stp = 1
+		stp += 1
 	def spam(self):
+		global stp
 		global i
+		stp = 1
 		if box.get() == 1 and at.get() == 1 and infinite.get() == 0:
 			l = i.get()
-			g = 1#2C2F33
+			g = 1
 			time.sleep(5)
 			while g <= l:
+				root.update()
+				if stp != 1:
+					break
+					stp = 1
+				root.update()
 				time.sleep(0.9)
+				root.update()
 				pyautogui.typewrite("@" + (tag.get()), interval=0)
 				pyautogui.typewrite(['enter'])
 				pyautogui.typewrite(['enter'])
+				root.update()
 				g += (1)
 		elif box.get() == 0 and at.get() == 0 and infinite.get() == 0:
 			l = i.get()
 			g = 1
 			time.sleep(5)
 			while g <= l:
+				root.update()
+				if stp != 1:
+					break
+					stp = 1
+				root.update()
 				pyautogui.typewrite((tag.get()), interval=0)
 				pyautogui.typewrite(['enter'])
 				pyautogui.typewrite(['enter'])
+				root.update()
 				g += (1)
 		elif box.get() == 1 and at.get() == 0 and infinite.get() == 0:
 			l = i.get()
 			g = 1
 			time.sleep(5)
 			while g <= l:
+				root.update()
+				if stp != 1:
+					break
+					stp = 1
+				root.update()
 				time.sleep(0.9)
+				root.update()
 				pyautogui.typewrite((tag.get()), interval=0)
 				pyautogui.typewrite(['enter'])
 				pyautogui.typewrite(['enter'])
+				root.update()
 				g += (1)
 		elif box.get() == 0 and at.get() == 1 and infinite.get() == 0:
 			while g <= l:
+				root.update()
+				if stp != 1:
+					break
+					stp = 1
+				root.update()
 				pyautogui.typewrite("@" + (tag.get()), interval=0)
 				pyautogui.typewrite(['enter'])
-				pyautogui.typewrite(['enter'])
+				pyautogui.typewrite(['eninit_window(self)ter'])
+				root.update()
 				g += (1)
 		elif box.get() == 1 and at.get() == 1 and infinite.get() == 1:
 			time.sleep(5)#2C2F33
 			while(True):
+				root.update()
+				if stp != 1:
+					break
+					stp = 1
+				root.update()
 				time.sleep(0.9)
+				root.update()
 				pyautogui.typewrite("@" + (tag.get()), interval=0)
 				pyautogui.typewrite(['enter'])
 				pyautogui.typewrite(['enter'])
+				root.update()
 				
 		elif box.get() == 0 and at.get() == 0 and infinite.get() == 1:
 			time.sleep(5)
 			while(True):
+				root.update()
+				if stp != 1:
+					break
+					stp = 1
+				root.update()
 				pyautogui.typewrite((tag.get()), interval=0)
 				pyautogui.typewrite(['enter'])
 				pyautogui.typewrite(['enter'])
+				root.update()
 		elif box.get() == 1 and at.get() == 0 and infinite.get() == 1:
 			time.sleep(5)
 			while(True):
+				root.update()
+				if stp != 1:
+					break
+					stp = 1
+				root.update()
 				time.sleep(0.9)
+				root.update()
 				pyautogui.typewrite((tag.get()), interval=0)
 				pyautogui.typewrite(['enter'])
 				pyautogui.typewrite(['enter'])
+				root.update()
 		elif box.get() == 0 and at.get() == 1 and infinite.get() == 1:
 			time.sleep(5)
 			while(True):
+				root.update()
+				if stp != 1:
+					break
+					stp = 1
+				root.update()
 				pyautogui.typewrite("@" + (tag.get()), interval=0)
 				pyautogui.typewrite(['enter'])
 				pyautogui.typewrite(['enter'])
+				root.update()
 root = Tk()
+
 
 tag = StringVar()
 
@@ -94,8 +156,7 @@ i = IntVar()
 
 infinite = IntVar()
 
-root.geometry("400x295")#2C2F33
-
+root.geometry("400x295")
 root.tk.call('wm', 'iconphoto', root._w, PhotoImage(file='myicon.gif'))
 
 app = Window(root)
